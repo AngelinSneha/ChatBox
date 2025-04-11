6583fc15d47698d7d9250941e46fb88c2a8d67a5
Angelin Sneha V <66509492+AngelinSneha@users.noreply.github.com>
2025-04-11 17:16:06 +0530

    Update messages.js

--- utils/messages.js
+++ utils/messages.js
@@ -1,5 +1,68 @@
 const moment = require("moment");
-const messageFormat = function(username, text) {
+function messageFormat(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  }
+}
+function messageFormat1(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  }
+}
+function messageFormat2(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  }
+}
+function messageFormat3(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  }
+}
+function messageFormat4(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  }
+}
+function messageFormat5(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  }
+}
+function messageFormat6(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  }
+}
+function messageFormat7(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  }
+}
+function messageFormat8(username, text) {
+  return {
+    username,
+    text,
+    time: moment().format("h:mm a")
+  }
+}
+function messageFormat9(username, text) {
   return {
     username,
     text,
